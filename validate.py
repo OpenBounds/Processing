b70012fb7b013084e62e73d2997b4acde26f2f42
Nathan Cahill <nathan@nathancahill.com>
2015-11-06 12:27:14 -0700

    add travis-ci build script - closes #6

--- validate.py
+++ validate.py
@@ -4,22 +4,26 @@ import json
 import click
 import jsonschema
 
+import utils
+
 
 @click.command()
 @click.argument('schema', type=click.File('r'), required=True)
-@click.argument('jsonfile', type=click.File('r'), required=True)
-def validate(schema, jsonfile):
-    """Validate a JSON file against a JSON schema.
+@click.argument('jsonfiles', type=click.Path(exists=True), required=True)
+def validate(schema, jsonfiles):
+    """Validate a JSON files against a JSON schema.
 
     \b
     SCHEMA: JSON schema to validate against. Required.
-    JSONFILE: JSON file to validate. Required.
+    JSONFILE: JSON files to validate. Required.
     """
-
     schema = json.loads(schema.read())
-    jsonfile = json.loads(jsonfile.read())
 
-    jsonschema.validate(jsonfile, schema)
+    for path in utils.get_files(jsonfiles):
+        with open(path) as f:
+            jsonfile = json.loads(f.read())
+
+        jsonschema.validate(jsonfile, schema)
 
 
 if __name__ == '__main__':
