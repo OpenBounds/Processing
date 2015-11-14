8b2f214eccfe6446c85a8b27814474f545c7156a
Nathan Cahill <nathan@nathancahill.com>
2015-11-13 22:34:54 -0700

    add bbox to geojson

--- adapters/shp.py
+++ adapters/shp.py
@@ -5,6 +5,27 @@ import fiona
 from fiona.transform import transform_geom
 
 
+def _explode(coords):
+    """Explode a GeoJSON geometry's coordinates object and
+    yield coordinate tuples. As long as the input is conforming,
+    the type of the geometry doesn't matter.
+
+    From @sgillies answer: http://gis.stackexchange.com/a/90554/27367
+    """
+    for e in coords:
+        if isinstance(e, (float, int, long)):
+            yield coords
+            break
+        else:
+            for f in _explode(e):
+                yield f
+
+
+def _bbox(feat):
+    x, y = zip(*list(_explode(feat['geometry']['coordinates'])))
+    return min(x), min(y), max(x), max(y)
+
+
 def _transformer(crs, feat):
     tg = partial(transform_geom, crs, 'EPSG:4326',
                  antimeridian_cutting=True, precision=6)
@@ -25,15 +46,22 @@ def read(fp):
     filename = '/' + layers[0] + '.shp'
 
     with fiona.open(filename, vfs='zip://' + fp.name) as source:
-        meta = source.meta
-        meta['fields'] = dict(source.schema['properties'].items())
-
         collection = {
             'type': 'FeatureCollection',
-            'fiona:schema': meta['schema'],
-            'fiona:crs': meta['crs'],
-            'features': [_transformer(source.crs, rec) for rec in source]
+            'features': [],
+            'bbox': [float('inf'), float('inf'), float('-inf'), float('-inf')]
         }
+
+        for rec in source:
+            transformed = _transformer(source.crs, rec)
+            collection['bbox'] = [
+                comparator(values)
+                for comparator, values in zip(
+                    [min, min, max, max],
+                    zip(collection['bbox'], _bbox(transformed))
+                )
+            ]
+            collection['features'].append(transformed)
 
     return collection
 
