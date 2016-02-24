05352852989fc9a9cd43a17ebd2a1aeb28bee4d5
Jesse Crocker <jesse@gaiagps.com>
2016-02-24 14:03:07 -0700

    read filenameInZip property from sources to handle zip files with multiple shapefiles

--- adapters/geojson.py
+++ adapters/geojson.py
@@ -1,7 +1,9 @@
 
-def read(url):
+def read(fp, prop_map, source_filename=None):
+    """Read GeoJSON file.
+
+    :param fp: file-like object
+    :param prop_map: dictionary mapping source properties to output properties
+    :param source_filename: Filename to read, only applicable if fp is a zip file
+    """
     return {}
-
-
-def write():
-    pass
--- adapters/shp.py
+++ adapters/shp.py
@@ -36,24 +36,27 @@ def _transformer(crs, feat):
     return feat
 
 
-def read(fp, prop_map):
+def read(fp, prop_map, source_filename=None):
     """Read shapefile.
 
     :param fp: file-like object
+    :param prop_map: dictionary mapping source properties to output properties
+    :param source_filename: Filename to read, only applicable if fp is a zip file
     """
     #search for a shapefile in the zip file, unzip if found
     unzip_dir = tempfile.mkdtemp()
-    shp_name = None
+    shp_name = source_filename
     with zipfile.ZipFile(fp.name, 'r') as zipped_file:
-        for name in zipped_file.namelist():
-            base, ext = os.path.splitext(name)
-            if ext == ".shp":
-                if shp_name is not None:
-                    raise Exception("Found multiple shapefiles in zipfile")
-                shp_name = name
-
         if shp_name is None:
-            raise Exception("Found 0 shapefiles in zipfile")
+            for name in zipped_file.namelist():
+                base, ext = os.path.splitext(name)
+                if ext == ".shp":
+                    if shp_name is not None:
+                        raise Exception("Found multiple shapefiles in zipfile")
+                    shp_name = name
+
+            if shp_name is None:
+                raise Exception("Found 0 shapefiles in zipfile")
 
         zipped_file.extractall(unzip_dir)
 
@@ -83,7 +86,3 @@ def read(fp, prop_map):
     shutil.rmtree(unzip_dir)
 
     return collection
-
-
-def write():
-    pass
