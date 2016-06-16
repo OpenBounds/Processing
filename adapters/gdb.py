28192f5d35fd7c058570be3fefe14f6e8294cdea
Jesse Crocker <jesse@gaiagps.com>
2016-06-16 09:52:17 -0600

    Don't require fileNameInZip to be specified for gdb

--- adapters/gdb.py
+++ adapters/gdb.py
@@ -18,6 +18,18 @@ def read(fp, prop_map, filterer=None, source_filename=None, layer_name=None):
     unzip_dir = tempfile.mkdtemp(suffix=".gdb")
     with zipfile.ZipFile(fp.name, 'r') as zipped_file:
         zipped_file.extractall(unzip_dir)
+        
+        if source_filename is None:
+           for name in zipped_file.infolist():
+                dirname = os.path.dirname(name.filename)
+                base, ext = os.path.splitext(dirname)
+                if ext == ".gdb":
+                    if source_filename is not None and source_filename != dirname:
+                        raise Exception("Found multiple .gdb entries in zipfile")
+                    source_filename = dirname
+
+        if source_filename is None:
+            raise Exception("Unabled to find .gdb directory in zipfile, and filenameInZip not set")
 
     #Open the shapefile
     with fiona.open(os.path.join(unzip_dir, source_filename), layer=layer_name) as source:
