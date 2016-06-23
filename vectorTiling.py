50804fc2e2626223a060983a2f6bc4f6e88d8b04
alexthi <alexandre.thissen@gmail.com>
2016-06-23 12:35:27 -0700

    do not use file as variable name, path to mbtiles instead of directory, use utils instead of os to stay consistent

--- vectorTiling.py
+++ vectorTiling.py
@@ -3,37 +3,36 @@ import json
 import click
 import subprocess
 import utils 
-import os 
 import logging
  
 @click.command()
 @click.argument('sources', type=click.Path(exists=True), required=True)
-@click.argument('output', type=click.Path(exists=True), required=True)
+@click.argument('output', required=True)
 @click.argument('min_zoom', default=5)
 @click.argument('max_zoom', default=14)
 def vectorTiling(sources, output, min_zoom, max_zoom):
     """ Function that creates vector tiles
     PARAMS: 
     	- sources : directory where the geojson file(s) are
-    	- output : directory for the generated data 
+    	- output : file.mbtiles for the generated data 
     """
+
     files = []
-    for file in utils.get_files(sources):
-    	if not os.path.isdir(file): 
-    		if file.split('.')[1] == 'geojson': 
-    			files.append(file)
+    for f in utils.get_files(sources):
+    	if utils.get_path_parts(f)[-1].split('.')[1] == 'geojson':
+            files.append(f)
 
 	logging.info("{} geojson found".format(len(files)))
 	paths_string = ''
 
-    for file in files: 
-    	with open(file, 'rb') as f: 
+    for item in files: 
+    	with open(item, 'rb') as f: 
         	geojson = json.load(f)
     	features = geojson['features']
     	for item in features: 
         	paths_string += item['properties']['path'] + ' '
 
-    command = 'tippecanoe -f -o ' + output + '/result.mbtiles ' + paths_string + ' -z {} -Z {}'.format(max_zoom, min_zoom)
+    command = 'tippecanoe -f -o ' + output + ' ' + paths_string + ' -z {} -Z {}'.format(max_zoom, min_zoom)
     subprocess.call(command,shell=True)
 
 if __name__ == '__main__':
