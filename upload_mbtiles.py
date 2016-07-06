2ea64443c55ae5afbd276f2f1e782ef28c366b6f
Jesse Crocker <jesse@gaiagps.com>
2016-07-06 07:40:00 -0600

    Script for uploading tiles to s3

---
+++ upload_mbtiles.py
@@ -0,0 +1,90 @@
+from __future__ import print_function
+
+import os
+from urlparse import urlparse
+import sqlite3
+from multiprocessing.pool import ThreadPool
+from functools import partial
+import cStringIO
+
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+from boto.exception import S3ResponseError
+import click
+
+import utils
+
+tile_count = 0
+upload_count = 0 
+
+class MBTilesGenerator(object):
+    """Generator that returns tiles from an mbtiles file"""
+    def __init__(self, mbtiles):
+        super(MBTilesGenerator, self).__init__()
+        self.db = sqlite3.connect(mbtiles)
+        self.cursor = self.db.cursor()
+        self.cursor.execute("SELECT zoom_level, tile_column, tile_row, tile_data FROM tiles")
+
+    def len(self):
+        c = self.db.cursor()
+        c.execute("SELECT count(1) from tiles")
+        return c.fetchone()[0]
+
+    def __iter__(self):
+        return self
+
+    # Python 3 compatibility
+    def __next__(self):
+        return self.next()
+
+    def next(self):
+        row = self.cursor.fetchone()
+        if row == None:
+            raise StopIteration()
+        zoom, x, y, tile = row
+        y  = ((1 << zoom) - y) - 1;
+        return zoom, x, y, tile
+
+
+def upload_tile(bucket, key_template, tile_stuff, progress=True):
+    zoom, x, y, tile = tile_stuff
+    k = Key(bucket)
+    k.key = key_template.format(z=zoom, x=x, y=y)
+    k.set_contents_from_string(str(tile))
+    global upload_count
+    upload_count += 1
+    if progress and upload_count % 10 == 0:
+        print("%i/%i" % (upload_count, tile_count))
+
+@click.command()
+@click.argument('mbtiles', type=click.Path(exists=True), required=True)
+@click.argument('s3_url', required=True)
+@click.option('--threads', default=10,
+    help="Number of simultaneous uploads")
+@click.option('--extension', default=".pbf",
+    help="File extension for tiles")
+def upload(mbtiles, s3_url, threads, extension):
+    """Upload tiles from an MBTiles file to S3.
+
+    \b
+    PARAMS:
+        mbtiles: Path to an MBTiles file
+        s3_url: url to an s3 bucket to upload tiles to
+    """
+    base_url = urlparse(s3_url)
+    conn = S3Connection()
+    bucket = conn.get_bucket(base_url.netloc)
+    key_prefix = base_url.path.lstrip("/")
+
+    tiles = MBTilesGenerator(mbtiles)
+    global tile_count
+    tile_count = tiles.len()
+
+    key_template = key_prefix + "{z}/{x}/{y}" + extension
+    pool = ThreadPool(threads)
+    func = partial(upload_tile, bucket, key_template)
+    pool.map(func, tiles)
+
+
+if __name__ == '__main__':
+    upload()
